0cd0c14c85e4c12ea2d6c6084b2b61f19864fc98
liangweibin <liangweibin1@banggood.com>
2021-03-12 17:10:54 +0800

    Add AspNetAuthFilter project

---
+++ AspNetAuthFilter/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetAuthFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            return Json("Hello world");
+        }
+
+        [AllowAnonymous]
+        public async Task<ActionResult> IndexAllowAnonymous()
+        {
+            return Json("Hello world");
+        }
+    }
+}
---
+++ AspNetAuthFilter/Filters/AuthorizationFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetAuthFilter.Filters
+{
+    public class AuthorizationFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var controllerActionDescriptor = context?.ActionDescriptor as ControllerActionDescriptor;
+
+            var boolAllowAnonymousClass = controllerActionDescriptor?.ControllerTypeInfo?.GetCustomAttributes(inherit: true).Any(a => a.GetType().Equals(typeof(AllowAnonymousAttribute)));
+            if (boolAllowAnonymousClass.HasValue && boolAllowAnonymousClass.Value)
+                return;
+
+            var boolAllowAnonymousMethod = controllerActionDescriptor?.MethodInfo?.GetCustomAttributes(inherit: true).Any(a => a.GetType().Equals(typeof(AllowAnonymousAttribute)));
+            if (boolAllowAnonymousMethod.HasValue && boolAllowAnonymousMethod.Value)
+                return;
+
+            context.Result = new Microsoft.AspNetCore.Mvc.JsonResult("No permissions");
+        }
+    }
+}
