85140006ba317c1ae2838071aafbc9b456833722
liangweibin <liangweibin1@banggood.com>
2021-03-23 17:04:46 +0800

    Add access MySql

---
+++ AspNetAccessMySql/DbContexts/erp_css.cs
@@ -0,0 +1,22 @@
+﻿using AspNetAccessMySql.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetAccessMySql.DbContexts
+{
+    /// <summary>
+    /// Database: erp_css
+    /// </summary>
+    public class erp_css : DbContext
+    {
+        /// <summary>
+        /// Table: test_user
+        /// </summary>
+        protected DbSet<test_user> test_user { get; set; }
+
+        public erp_css(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ AspNetAccessMySql/Models/test_user.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetAccessMySql.Models
+{
+    public class test_user
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public DateTime? age { get; set; }
+    }
+}
