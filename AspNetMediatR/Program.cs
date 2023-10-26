fff950587343b28584577acad1f8f1f13f1e7296
AndyLeung <407833635@qq.com>
2023-10-26 11:30:10 +0800

    Update AspNetMediatR

--- AspNetMediatR/Program.cs
+++ AspNetMediatR/Program.cs
@@ -1,16 +1,20 @@
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddMediatR(typeof(Program));
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssemblyContaining(typeof(Program));
+    cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
-var mediator = app.Services.GetService<IMediator>();
+var mediator = app.Services.GetRequiredService<IMediator>();
 
 var r = await mediator.Send(new Ping { Id = 1 });
 await mediator.Publish(new Pinged { Id = 2 });
 
-app.MapGet("/", () => $"Ping's id is {r}");
+app.MapGet("/", async (IMediator m) => { await m.Send(new Ping { Id = 1 }); return $"Ping's id is {r}"; });
 
 app.Run();
 
@@ -48,6 +52,22 @@ public class PingedHandler2 : INotificationHandler<Pinged>
     {
         Console.WriteLine($"{DateTime.Now} Handler:{nameof(PingedHandler2)}, Ping's id is {notification.Id}");
         return Task.FromResult(notification.Id);
+    }
+}
+#endregion
+
+#region IPipelineBehavior
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger) => _logger = logger;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"{DateTime.Now} LoggingBehavior start.");
+        var response = await next();
+        _logger.LogInformation($"{DateTime.Now} LoggingBehavior end.");
+        return response;
     }
 }
 #endregion
