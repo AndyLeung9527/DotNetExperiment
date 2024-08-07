8d9547600bf5250e490f1f465ea7e8ff308f802a
AndyLeung <407833635@qq.com>
2024-08-07 11:41:10 +0800

    Add DotNetSocket

---
+++ DotNetSocket/DotNetSocketClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DotNetSocketClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
+
+            // Send message
+            using Socket client = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            await client.ConnectAsync(ipEndPoint);
+            var sendMessage = "Hello world!";
+            var sendMessageBytes = Encoding.UTF8.GetBytes(sendMessage);
+            await client.SendAsync(sendMessageBytes);
+            Console.WriteLine($"发送：{sendMessage}");
+
+            // Receive message
+            var buffer = new byte[1024];
+            var receivedLength = await client.ReceiveAsync(buffer, SocketFlags.None);
+            var receivedMessage = Encoding.UTF8.GetString(buffer, 0, receivedLength);
+            Console.WriteLine($"接收：{receivedMessage}");
+
+            client.Shutdown(SocketShutdown.Both);
+        }
+    }
+}
---
+++ DotNetSocket/DotNetSocketServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DotNetSocketServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
+
+            using Socket listener = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(ipEndPoint);
+            listener.Listen(100);
+
+            var handler = await listener.AcceptAsync();
+
+            // Receive message
+            var buffer = new byte[1024];
+            var receivedLength = await handler.ReceiveAsync(buffer, SocketFlags.None);
+            var receivedMessage = Encoding.UTF8.GetString(buffer, 0, receivedLength);
+            Console.WriteLine($"接收：{receivedMessage}");
+
+            // Response message
+            var responseMessage = "Hello world, too";
+            var responseMessageBytes = Encoding.UTF8.GetBytes(responseMessage);
+            await handler.SendAsync(responseMessageBytes);
+            Console.WriteLine($"响应：{responseMessage}");
+        }
+    }
+}
