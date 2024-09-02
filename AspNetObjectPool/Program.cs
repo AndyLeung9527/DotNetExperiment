47794093e1e6b848c994bdb1ee4742dae26e97eb
AndyLeung <407833635@qq.com>
2024-09-02 10:24:08 +0800

    Add AspNetObjectPool

---
+++ AspNetObjectPool/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace AspNetObjectPool;
+
+using Microsoft.Extensions.ObjectPool;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+        builder.Services.AddSingleton(sp =>
+        {
+            var provider = sp.GetRequiredService<ObjectPoolProvider>();
+            return provider.Create(new StringBuilderPooledObjectPolicy());
+        });
+
+        var app = builder.Build();
+
+        app.MapGet("/", (ObjectPool<StringBuilder> pool) =>
+        {
+            var sb = pool.Get();
+            try
+            {
+                sb.Append("Hello, World!");
+                return sb.ToString();
+            }
+            finally
+            {
+                pool.Return(sb);
+            }
+        });
+
+        app.Run();
+    }
+}
+
+public class StringBuilderPooledObjectPolicy : PooledObjectPolicy<StringBuilder>
+{
+    public override StringBuilder Create()
+    {
+        return new StringBuilder();
+    }
+
+    public override bool Return(StringBuilder obj)
+    {
+        obj.Clear();
+        return true;
+    }
+}
