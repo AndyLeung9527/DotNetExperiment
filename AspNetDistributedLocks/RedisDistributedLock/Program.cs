9cb4ac0766139b11f1f1c03ce7f6b78670d1bedb
AndyLeung <407833635@qq.com>
2024-09-23 14:37:19 +0800

    Update AspNetDistributedLocks

--- AspNetDistributedLocks/RedisDistributedLock/Program.cs
+++ AspNetDistributedLocks/RedisDistributedLock/Program.cs
@@ -1,13 +1,74 @@
-﻿using StackExchange.Redis;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
+namespace RedisDistributedLock;
 
-var connectionMultiplexer = ConnectionMultiplexer.Connect("192.168.2.113:6379");
-builder.Services.AddSingleton(connectionMultiplexer);
+public class Program
+{
+    static long seed = 0;
 
-var app = builder.Build();
-app.UseRouting();
-app.MapDefaultControllerRoute();
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+        builder.Services.AddRedisLocker(options =>
+        {
+            options.EndPoints.Add("127.0.0.1", 6379);
+            options.AbortOnConnectFail = false;
+        });
+
+        var app = builder.Build();
+
+        app.MapGet("/stock/{stock:long}", SettingStockAsync);
+        app.MapGet("/consume", ConsumeAsync);
+
+        app.Run();
+    }
+
+    /// <summary>
+    /// 初始化库存
+    /// </summary>
+    /// <param name="stock">库存数量</param>
+    /// <returns></returns>
+    public static async Task<Results<Ok<string>, BadRequest<string>>> SettingStockAsync([FromRoute] long stock, [FromServices] IOptions<ConfigurationOptions> options)
+    {
+        var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(options.Value);
+        var settingResult = await connectionMultiplexer.GetDatabase(default).StringSetAsync("StockNum", stock);
+        if (settingResult)
+            return TypedResults.Ok($"设置库存数量{stock}");
+        else
+            return TypedResults.BadRequest("设置库存失败");
+    }
+
+    /// <summary>
+    /// 秒杀
+    /// </summary>
+    /// <returns></returns>
+    public static async Task<Results<Ok<string>, BadRequest<string>>> ConsumeAsync([FromServices] RedisLocker redisLocker, [FromServices] IOptions<ConfigurationOptions> options)
+    {
+        var id = $"{Environment.CurrentManagedThreadId}:{Interlocked.Increment(ref seed)}";//需要生成唯一标识，真实的生产环境可使用分布式雪花id
+        var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(options.Value);
+
+        //获取库存
+        long.TryParse(await connectionMultiplexer.GetDatabase(default).StringGetAsync("StockNum"), out var stockNum);
+        if (stockNum < 1) return TypedResults.BadRequest($"秒杀失败，库存不足，id{id}");
+
+        //获取redis分布式锁
+        using var locker = await redisLocker.LockAsync("StockLock", id, TimeSpan.FromSeconds(3), true);
+
+        //重新获取库存(因为采用双if+lock锁模式)
+        long.TryParse(await connectionMultiplexer.GetDatabase(default).StringGetAsync("StockNum"), out stockNum);
+        if (stockNum < 1) return TypedResults.BadRequest($"秒杀失败，库存不足，id{id}");
+
+        //扣减库存
+        stockNum -= 1;
+        var settingResult = await connectionMultiplexer.GetDatabase(default).StringSetAsync("StockNum", stockNum);
+        if (!settingResult) return TypedResults.BadRequest("设置库存失败");
+
+        //释放redis分布式锁
+        var releaseResult = await locker.ReleaseAsync();
+        return TypedResults.Ok($"秒杀成功，id{id}，解锁{(releaseResult.Succeeded ? "成功" : "失败")}，锁{(releaseResult.Released ? "已" : "未")}释放，剩余库存{stockNum}");
+    }
+}
---
+++ AspNetDistributedLocks/RedisDistributedLock/RedisLocker.cs
@@ -0,0 +1,177 @@
+﻿namespace RedisDistributedLock;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
+
+public class RedisLocker : IDisposable
+{
+    private readonly IConnectionMultiplexer _connectionMultiplexer;
+    private readonly string _acquireScript;
+
+    public RedisLocker(IOptions<ConfigurationOptions> options)
+    {
+        _connectionMultiplexer = ConnectionMultiplexer.Connect(options.Value);
+        _acquireScript = """
+            -- 若锁不存在，则新增锁并设置：1.唯一标识，2.重入计数(初始值为1)，3.锁过期时间
+            if (redis.call('exists', @key) == 0) then
+                redis.call('hset', @key, @id, 1);
+                redis.call('pexpire', @key, @expire);
+                return nil;
+            end;
+            -- 若锁存在且唯一标识匹配，设置：1.重入计数+1，2.锁过期时间
+            if (redis.call('hexists', @key, @id) == 1) then
+                redis.call('hincrby', @key, @id, 1);
+                redis.call('pexpire', @key, @expire);
+                return nil;
+            end;
+            -- 若锁存在但唯一标识不匹配，即获取锁失败，返回锁剩余过期时间
+            return redis.call('pttl', @key);
+            -- 当且仅当返回nil，才表示获取锁成功
+            """;
+    }
+
+    /// <summary>
+    /// 获取锁
+    /// </summary>
+    /// <param name="key">锁名</param>
+    /// <param name="id">唯一标识</param>
+    /// <param name="expire">过期时长</param>
+    /// <param name="autoRenewal">自动续约</param>
+    /// <returns></returns>
+    public async Task<Locker> LockAsync(string key, string id, TimeSpan expire, bool autoRenewal = false)
+    {
+        var cts = new CancellationTokenSource();
+        while (true)
+        {
+            var acquireResult = await _connectionMultiplexer.GetDatabase(default).ScriptEvaluateAsync(LuaScript.Prepare(_acquireScript), new
+            {
+                key,
+                id,
+                expire = Convert.ToInt64(expire.TotalMilliseconds),
+            });
+            if (acquireResult.IsNull)
+            {
+                if (autoRenewal)
+                {
+                    Task.Run(async () =>
+                    {
+                        var delay = expire / 2;
+                        await Task.Delay(delay);
+                        while (!cts.Token.IsCancellationRequested)
+                        {
+                            var renewalScript = LuaScript.Prepare("""
+                                -- 若锁存在则重新设置过期时间
+                                if (redis.call('hexists', @key, @id) == 1) then
+                                    redis.call('pexpire', @key, @expire);
+                                    return 1;
+                                end;
+                                return 0;
+                                """);
+                            var renewalResult = await _connectionMultiplexer.GetDatabase(default).ScriptEvaluateAsync(renewalScript, new
+                            {
+                                key,
+                                id,
+                                expire = Convert.ToInt64(expire.TotalMilliseconds),
+                            });
+                            if (!renewalResult.IsNull && (int)renewalResult == 1) await Task.Delay(delay);
+                            else break;
+                        }
+                    }, cts.Token);
+                }
+                return new Locker(key, id, expire, _connectionMultiplexer, cts);
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        _connectionMultiplexer.Dispose();
+    }
+}
+
+public class Locker : IDisposable
+{
+    public string Key { get; }
+
+    public string Id { get; }
+
+    public TimeSpan Expire { get; }
+
+    private readonly IConnectionMultiplexer _connectionMultiplexer;
+
+    private readonly CancellationTokenSource _cts;
+
+    private static string RELEASE_SCRIPT = """
+            -- 若锁不存在或唯一标识不匹配则返回nil
+            if (redis.call('hexists', @key, @id) == 0) then
+                return nil;
+            end;
+            -- 否则重入计数-1
+            local counter = redis.call('hincrby', @key, @id, -1);
+            if (counter > 0) then
+                redis.call('pexpire', @key, @expire);
+                return 0;
+            else
+                -- 重入计数为0则删除锁
+                redis.call('del', @key);
+                return 1;
+            end;
+            return nil;
+            """;
+
+    public Locker(string key, string id, TimeSpan expire, IConnectionMultiplexer connectionMultiplexer, CancellationTokenSource cts)
+    {
+        Key = key;
+        Id = id;
+        Expire = expire;
+        _connectionMultiplexer = connectionMultiplexer;
+        _cts = cts;
+    }
+
+    /// <summary>
+    /// 释放锁
+    /// </summary>
+    /// <returns>item1: 是否成功, item2: 锁是否被完全释放</returns>
+    /// <exception cref="ObjectDisposedException"></exception>
+    public async Task<(bool Succeeded, bool Released)> ReleaseAsync()
+    {
+        if (!_cts.IsCancellationRequested)
+        {
+            var releaseResult = await _connectionMultiplexer.GetDatabase(default).ScriptEvaluateAsync(LuaScript.Prepare(RELEASE_SCRIPT), new
+            {
+                key = Key,
+                id = Id,
+                expire = Convert.ToInt64(Expire.TotalMilliseconds)
+            });
+            await _cts.CancelAsync();
+            _cts.Dispose();
+            return (!releaseResult.IsNull, releaseResult.IsNull ? false : (int)releaseResult == 1);
+        }
+        throw new ObjectDisposedException("重复释放");
+    }
+
+    public void Dispose()
+    {
+        if (!_cts.IsCancellationRequested)
+        {
+            _connectionMultiplexer.GetDatabase(default).ScriptEvaluate(LuaScript.Prepare(RELEASE_SCRIPT), new
+            {
+                key = Key,
+                id = Id,
+                expire = Convert.ToInt64(Expire.TotalMilliseconds)
+            });
+            _cts.Cancel();
+            _cts.Dispose();
+        }
+    }
+}
+
+public static class RedisLockerExtensions
+{
+    public static void AddRedisLocker(this IServiceCollection services, Action<ConfigurationOptions> configure)
+    {
+        services.AddOptions().Configure(configure);
+        services.AddSingleton<RedisLocker>();
+    }
+}
